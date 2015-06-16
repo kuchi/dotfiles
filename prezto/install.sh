7e765d1e57d6bef8000cf2a6e7e95ca6c3faf70b
Anthony Tanbakuchi <anthony@tanbakuchi.com>
2015-06-15 22:41:46 -0600

    fix prezto

--- prezto/install.sh
+++ prezto/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Install oh-my-zsh
-if [ -d "$HOME/.oh-my-zsh/" ]
+if [ -d "$HOME/.prezto/" ]
 then
-  echo "oh-my-zsh already installed, skipping installation"
+  echo "Prezto already installed, skipping installation"
 else
-  echo "Installing oh-my-zsh..."
-  # curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+  echo "Installing prezto..."
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
