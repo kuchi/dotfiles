954e11e9dc4fa849cd36c7d80fc223a3d0ca0791
Anthony Tanbakuchi <anthony@tanbakuchi.com>
2015-06-28 12:14:23 -0600

    aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,11 @@ alias zshrc="atom ~/.dotfiles"
 alias ref-notes="atom ~/dev/ref-notes"
 alias 1535="cd /Volumes/Collaborative/"
 
+# Global alias for dl, can be anywhere in command
 alias -g dl="~/Downloads"
+
+# Quick search
+# f png  # Shows all png files in currend directory recursively
+function f() { find . -iname "*$1*" ${@:2} }
+# r HTTP  # Find all files with HTTP in them in current dir recursively
+function r() { grep "$1" ${@:2} -R . }
