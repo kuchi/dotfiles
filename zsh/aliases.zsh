94105ca68f5e5ce49a98355db1064403c807ff54
Tanbakuchi <atanbak@s984487.srn.sandia.gov>
2015-08-06 09:39:37 -0600

    alias update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 alias reload!='. ~/.zshrc'
 alias zshrc="atom ~/.dotfiles"
 alias ref-notes="atom ~/dev/ref-notes"
-alias 1535="cd /Volumes/Collaborative/"
+# alias 1535="cd /Volumes/Collaborative/"
 
 # Global alias for dl, can be anywhere in command
 alias -g dl="~/Downloads"
@@ -14,3 +14,20 @@ function r() { grep "$1" ${@:2} -R . }
 
 # Multi move
 alias mmv='noglob zmv -W'
+
+# Overwrite prezto cdf, it doesn't work with my network shares
+function cdf2 () {
+   currFolderPath=$( /usr/bin/osascript <<"         EOT"
+       tell application "Finder"
+           try
+               set currFolder to (folder of the front window as alias)
+           on error
+               set currFolder to (path to desktop folder as alias)
+           end try
+           POSIX path of currFolder
+       end tell
+         EOT
+   )
+   echo "cd to \"$currFolderPath\""
+   cd "$currFolderPath"
+}
